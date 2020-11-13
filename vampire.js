1aa1e07b99a507ec50e3496ede60dc5a918e15ac
Alberto Olvera Sotres <mty2yyz@gmail.com>
2020-11-13 18:26:34 -0500

    Intro to trees exercise

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let count = 0;
+    let currVamp = this;
+    while (currVamp.creator) {
+      currVamp = currVamp.creator;
+      count++;
+    }
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,7 +43,17 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let currVamp1 = this;
+    while (currVamp1) {
+      let currVamp2 = vampire;
+      while (currVamp2) {
+        if (currVamp1 === currVamp2) {
+          return currVamp1;
+        }
+        currVamp2 = currVamp2.creator;
+      }
+      currVamp1 = currVamp1.creator;
+    }
   }
 }
 
